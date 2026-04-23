eae5fa8fe5b7457bc99cf8676933550d1b83e28f
oscitychrisw <oscitychris@gmail.com>
2026-04-23 12:52:18 -0500

    Delete schedule.js

--- schedule.js
+++
@@ -1,9 +0,0 @@
-export const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-export const SHIFTS = ["AM", "PM", "Overnight"];
-export const SLOT_CODES = [
-  "shift_supervisor",
-  "crystallization_1",
-  "crystallization_2",
-  "solvent_recovery",
-  "matrix_washing",
-];
