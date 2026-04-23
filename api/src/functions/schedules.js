070f4d578f83e89fc53b223005658a7f3b24e4ac
oscitychrisw <oscitychris@gmail.com>
2026-04-23 13:51:22 -0500

    Update schedules.js

--- api/src/functions/schedules.js
+++ api/src/functions/schedules.js
@@ -1,10 +1,26 @@
 import { app } from "@azure/functions";
+import { getSchedule, saveSchedule } from "../shared/repository.js";
+import { scheduleSchema } from "../shared/validation.js";
 
-app.http("schedules", {
+app.http("getSchedule", {
   methods: ["GET"],
   authLevel: "anonymous",
   route: "schedules/{weekKey}",
-  handler: async (req) => {
-    return { jsonBody: { week: req.params.weekKey } };
+  handler: async (request) => {
+    const weekKey = request.params.weekKey || "current";
+    const assignments = await getSchedule(weekKey);
+    return { jsonBody: { weekKey, assignments } };
+  }
+});
+
+app.http("saveSchedule", {
+  methods: ["PUT"],
+  authLevel: "anonymous",
+  route: "schedules/{weekKey}",
+  handler: async (request) => {
+    const payload = scheduleSchema.parse(await request.json());
+    const weekKey = request.params.weekKey || payload.weekKey;
+    const assignments = await saveSchedule(weekKey, payload.assignments, "system");
+    return { jsonBody: { weekKey, assignments } };
   }
 });
